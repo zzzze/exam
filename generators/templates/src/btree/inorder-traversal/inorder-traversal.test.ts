162edf863ad107626aa2468aded4149eeb04796e
zeshuan <zhangzs000@hotmail.com>
2022-03-03 14:23:56 +0800

    Fixed unit test.

--- generators/templates/src/btree/inorder-traversal/inorder-traversal.test.ts
+++ generators/templates/src/btree/inorder-traversal/inorder-traversal.test.ts
@@ -3,7 +3,7 @@ import inorder from './inorder-traversal'
 
 const testCases = [{
   input: [1, null, 2, 3],
-  output: [1, 2, 3],
+  output: [1, 3, 2],
 }, {
   input: [3, 9, 20, null, null, 15, 7],
   output: [9, 3, 15, 20, 7],
